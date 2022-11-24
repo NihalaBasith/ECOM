13e50aa1c871288cbd7c008548ea64ee5482eea0
NihalaBasith <nihalabasith@icloud.com>
2022-11-24 16:40:08 +0530

    cart and order placing

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\products;
 use App\Models\cart;
+use App\Models\orders;
 
 use Session;
 use Illuminate\Support\Facades\DB;
@@ -51,15 +52,59 @@ class ProductController extends Controller
      $userId=Session::get('user')['id'];
      return Cart::where('user_id',$userId)->count();
     }
-    // function cartList()
+    function cartList()
+    {
+        $userId=Session::get('user')['id'];
+        $products= DB::table('carts')
+        ->join('products','carts.product_id','=','products.id')
+        ->where('carts.user_id',$userId)
+        ->select('products.*','carts.id as carts_id')
+        ->get();
+
+        return view('cartlist',['products'=>$products]);
+    }
+    function removeCart($id)
+    {
+        Cart::destroy($id);
+        return redirect('cartlist');
+    }
+    function orderNow()
+    {
+        $userId=Session::get('user')['id'];
+        $total= $products= DB::table('carts')
+         ->join('products','carts.product_id','=','products.id')
+         ->where('carts.user_id',$userId)
+         ->sum('products.price');
+ 
+         return view('ordernow',['total'=>$total]);
+    }
+    function orderPlace(Request $req)
+    {
+        $userId=Session::get('user')['id'];
+         $allCart= Cart::where('user_id',$userId)->get();
+         foreach($allCart as $cart)
+         {
+             $order= new orders;
+             $order->product_id=$cart['product_id'];
+             $order->user_id=$cart['user_id'];
+             $order->status="pending";
+             $order->payment_method=$req->payment;
+             $order->payment_status="pending";
+             $order->address=$req->address;
+             $order->save();
+             Cart::where('user_id',$userId)->delete(); 
+         }
+         $req->input();
+         return redirect('/');
+    }
+    // function myOrders()
     // {
     //     $userId=Session::get('user')['id'];
-    //     $products= DB::table('cart')
-    //     ->join('products','cart.product_id','=','products.id')
-    //     ->where('cart.user_id',$userId)
-    //     ->select('products.*','cart.id as cart_id')
-    //     ->get();
-
-    //     return view('cartlist',['products'=>$products]);
+    //     $orders= DB::table('orders')
+    //      ->join('products','orders.product_id','=','products.id')
+    //      ->where('orders.user_id',$userId)
+    //      ->get();
+ 
+    //      return view('myorders',['orders'=>$orders]);
     // }
 }
