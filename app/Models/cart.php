80566da81fbf34147990f0941a1847eb2e72b5da
NihalaBasith <nihalabasith@icloud.com>
2022-11-24 15:52:02 +0530

    login and logout

--- app/Models/cart.php
+++ app/Models/cart.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class products extends Model
+class cart extends Model
 {
     use HasFactory;
-    
+    public $table ="carts"; 
 }
